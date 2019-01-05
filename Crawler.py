5044ce6134291d4bde8e4ad62cb7cba88988fb97
arpit <uarpit@gmail.com>
2019-01-05 13:02:52 -0500

    Added comments and cleaned up the code

--- Crawler.py
+++ Crawler.py
@@ -16,67 +16,74 @@ class Crawler:
         self.webDriver = webdriver.Chrome()
         self.industries = []
         self.industries_href = []
-        self.wait_df = 10
-        self.wait = WebDriverWait(self.webDriver, self.wait_df)
+        self.wait = WebDriverWait(self.webDriver, 10)
         self.element_list = []
         self.s = Stocks.Stock()
 
+    # This function waits for the given xpath to become visible and clickable
     def wait_by_xpath(self, xpath):
         self.wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
         self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
 
+    # This method loads the industry list with dictionaries [Title : href]
+    # It takes the xpath as an argument from 'crawl' function
     def load_industries(self, xpath):
+        print("Loading list of all industries...")
         self.element_list = []
+        # Creates list of all web elements for each industry
         self.element_list = self.webDriver.find_elements_by_xpath(xpath)
+
         for i in self.element_list:
             title = i.get_attribute('title')
             href = i.get_attribute('href')
-            key_val = {title: href}
-            self.industries.append(key_val)
+            key_val = {title: href}          # Create a dictionary
+            self.industries.append(key_val)  # append it to the list
 
-        # adding test breakpoint
-        print("{}".format(self.industries))
-        #self.industries = ['Technology']
+        print("Industries data updated.")
 
+    # Extracts stocks information for a given file/Industry.
+    # It takes offset as an argument which ensures that the header
+    # is written only once in the file for multiple pages of stocks
     def extract_stocks(self, file, offset):
-
         print("Stock information being written to file : {}".format(file))
-        #Create a file even if it doesnot exists (option a+)
-        with open(file, 'a+') as f:
 
-            #row_number = 1
+        # Create a file even if it does not exists (option a+)
+        with open(file, 'a+') as f:
+            # This xpath gives list of each row on the summary page of the selected industry
             xpath = "//table[@class='W(100%)']//child::a"
             self.element_list = []
             self.wait_by_xpath(xpath)
             self.element_list = self.webDriver.find_elements_by_xpath(xpath)
             print("Generated element list from the stock table")
+
             print("Loading stock object from stock element details..")
             for e in self.element_list:
                 self.s.ticker = e.text
                 self.s.name = e.get_attribute('title')
                 self.wait_by_xpath(xpath)
 
-                # this will directly convert an class object attributes into dictionary
+                # this will directly convert the Stock class object attributes into dictionary
                 row_dict = self.s.__dict__
+                # Writes dictionary into row in the file
                 w = csv.DictWriter(f, row_dict.keys())
                 if offset == 0:
                     w.writeheader()
                     offset += 1
                 w.writerow(row_dict)
-                #row_number += 1
 
-            #print("{} rows writen to {}".format(row_number,file))
-
-
-
-
+    # Starts crawling form the given homepage of Yahoo finance.
+    # Collects all the industry details from the homepage
+    # Clears all the contents from the output file directory, removes and recreates the directory
+    # Creates csv file per industry in the output folder
+    # Iterates through pages for each industry
+    # Opens the first page of the industry, collects count of stocks
+    # Iterates through each page in the industry until offset is more than the number of stocks captured
     def crawl(self, url):
         self.webDriver.maximize_window()
-        self.webDriver.get(url)
+        self.webDriver.get(url)                 # open the homepage
 
-        # Get the titles of all industries in yahoo finance
+        # Get the titles of all industries in yahoo finance and load it in the list
         self.webDriver.find_element_by_xpath("//a[@title='Industries']").click()
-
         xpath = "//ul[@data-test='secnav-list']//child::a"
         self.wait_by_xpath(xpath)
         self.load_industries(xpath)
@@ -93,6 +100,7 @@ class Crawler:
         # iterate through pages for each industry
         for i in self.industries:
 
+            # expects only one key as the industry name same a file name
             file = ''.join(list(i.keys()))
             #create full path of the output file for each industry
             #print("element is {}".format(file))
@@ -102,15 +110,19 @@ class Crawler:
 
             href = i[file]
 
-            offset =0
+            offset = 0
             while True:
+                # Ex: https://finance.yahoo.com/sector/financial?offset=0&count=200
+                # Ex: https://finance.yahoo.com/sector/technology?offset=200&count=200
                 href_itr = href + "?offset=" + str(offset) + "&count=200"
                 self.webDriver.get(href_itr)
                 self.extract_stocks(full_path, offset)
 
                 if offset == 0:
+                    # extract text label which shows total stocks
                     xpath = "//span[@class='Mstart(15px) Fw(500) Fz(s) Mstart(0)--mobp Fl(start)--mobp']//child::*"
                     textLable = self.webDriver.find_element_by_xpath(xpath).text
+                    # third string in the label has the number of stocks
                     stocks_count = int(textLable.split(" ")[2])
                     print("{} has {} Stocks".format(file, stocks_count))
                     print("Scraping stocks in {} industry".format(file))
@@ -120,53 +132,6 @@ class Crawler:
                 if offset >= stocks_count:
                     break
 
-
-
-                # [div, mod] = divmod(stocks_count, 200)
-                # print("Div : {} and Rem: {}".format(div, mod))
-
-                # if mod != 0:
-                #     itr = div
-                # else:
-                #     itr = div - 1
-
-                # offset = 200
-                # for i in range(0, itr):
-
-
-
-
-            # try:
-            #     xpath = "//a[@title='Industries']"
-            #     self.wait_by_xpath(xpath)
-            #     self.webDriver.find_element_by_xpath(xpath).click()
-            # except:
-            #     print("Exception: While clicking Industries button on main page during {} phase".format(i))
-            #
-            # try:
-            #     xpath = "//ul[@data-test='secnav-list']//child::a"
-            #     self.wait_by_xpath(xpath)
-            #     xpath = xpath + '[@title = \'' + i + '\']'
-            #     self.webDriver.find_element_by_xpath(xpath).click()
-            # except:
-            #     print("Exception: While clicking {} industry".format(i))
-
-            # try:
-            #     xpath = "//div[@data-test='select-container']"
-            #     self.wait_by_xpath(xpath)
-            #     self.webDriver.find_element_by_xpath(xpath).click()
-            #     self.webDriver.find_elements_by_xpath()
-            #     select_options = select_box.options
-            #         #deselect_by_visible_text('Show 100 rows')
-            #     print("Dropdown has {} options".format(select_options))
-            # except:
-            #     print("No drop downs")
-            #     pass
-
-
-
-
-
-
-c = Crawler()
-c.crawl('http://www.finance.yahoo.com')
+if __name__ == "__main__":
+    c = Crawler()
+    c.crawl('http://www.finance.yahoo.com')
--- Stocks.py
+++ Stocks.py
@@ -1,31 +1,30 @@
+# This class only acts as data structure to capture all stock details for each stock
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 class Stock:
 
     def __init__(self):
-        self.ticker = ''
-        self.name = ''
-        self.market_cap = ''
-        self.forward_pe = 0.0
-        self.trailing_pe = 0.0
-        self.avg_eps_curr_yr = 0.0
-        self.avg_eps_next_yr = 0.0
-        self.avg_eps_prev_yr = 0.0
-        self.rev_est_curr_yr = 0
-        self.rev_est_next_yr = 0
-        self.rev_est_prev_yr = 0
-        self.sls_grwth_curr_yr = 0.0
-        self.sls_grwth_next_yr = 0.0
+        self.ticker = ''                         # Ticker name
+        self.name = ''                           # Full name of the Stock
+        self.market_cap = ''                     # Market capital
+        self.forward_pe = 0.0                    # Forward PE
+        self.trailing_pe = 0.0                   # Trailing PE
+        self.avg_eps_curr_yr = 0.0               # Average EPS for current year
+        self.avg_eps_next_yr = 0.0               # Average EPS for next year
+        self.avg_eps_prev_yr = 0.0               # Average EPS for previous year
+        self.rev_est_curr_yr = 0                 # Revenue estimates for current year
+        self.rev_est_next_yr = 0                 # Revenue estimates for next year
+        self.rev_est_prev_yr = 0                 # Revenue estimates for previous year
+        self.sls_grwth_curr_yr = 0.0             # Sales growth current year
+        self.sls_grwth_next_yr = 0.0             # Sales growth next year
 
+    # Calculates EPS change in current year vs previous year
     def avg_eps_chng_curr_yr(self):
         chng = (self.avg_eps_curr_yr - self.avg_eps_prev_yr) * 100 / self.avg_eps_prev_yr
         chng_rnd = round(chng, 2)
         return str(chng_rnd) + '%'
 
+    # Calculates EPS change in next year vs current year
     def avg_eps_chng_next_yr(self):
         chng = (self.avg_eps_next_yr - self.avg_eps_curr_yr) * 100 / self.avg_eps_next_yr
         chng_rnd = round(chng, 2)
